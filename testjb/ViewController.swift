27b3cde5fb17ee9f37c7123fdf5dc3fc80621a50
berna <bhernandez@consistent.com.mx>
2018-01-02 15:45:20 -0600

    Initial Commit

---
+++ testjb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testjb
+//
+//  Created by bernardo Hernández Ramírez on 02/01/18.
+//  Copyright © 2018 bernardo Hernández Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
