083e29c6650a2ce1e584244fc6e8757fce961043
berna <bhernandez@consistent.com.mx>
2018-01-03 12:35:12 -0600

    Primer Commit
    Desarrollo de aplicacion switf

--- testjb/ViewController.swift
+++ testjb/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var titleIv: UIImageView!
+    @IBOutlet weak var btn_presionar: UIButton!
+    var isActive = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.backgroundImageView.isHidden = true
+        self.titleIv.isHidden = true
+        self.btn_presionar.isHidden = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btn_press(_ sender: UIButton) {
+        if isActive {
+            self.backgroundImageView.isHidden = false
+            self.titleIv.isHidden = false
+            isActive = false
+        } else {
+            self.backgroundImageView.isHidden = true
+            self.titleIv.isHidden = true
+            isActive = true
+        }
+        
+       
+    }
+    
 }
 
